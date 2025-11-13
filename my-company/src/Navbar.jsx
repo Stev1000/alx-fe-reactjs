b8d3096716fd05384431d58ec54435e67f134378
Stevo <nsanzestevo1@gmail.com>
2025-11-14 00:02:27 +0200

    feat: build company website with react router and inline styling

---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav 
+      style={{
+        padding: '10px',
+        backgroundColor: '#333',
+        color: 'white'
+      }}
+    >
+      <Link to="/" style={{ marginRight: '15px', color: 'white' }}>Home</Link>
+      <Link to="/about" style={{ marginRight: '15px', color: 'white' }}>About</Link>
+      <Link to="/services" style={{ marginRight: '15px', color: 'white' }}>Services</Link>
+      <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;

ec77e9a7772e37aab969fb0794710ba63bf0567d
Stevo <nsanzestevo1@gmail.com>
2025-11-16 18:04:24 +0200

    final: refactor prop drilling using Context API exactly as instructions

--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,8 +1,7 @@
-// UserInfo.jsx
-import UserProfile from './UserProfile';
+import UserDetails from './UserDetails';
 
 function UserInfo() {
-  return <UserProfile />;
+  return <UserDetails />;
 }
 
 export default UserInfo;

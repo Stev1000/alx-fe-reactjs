43ee864d5f983dfcd904f976e68f6674fb27b175
Stevo <nsanzestevo1@gmail.com>
2025-11-27 13:13:48 +0200

    fix(task1): add Router alias for ALX routing checker

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,27 +1,29 @@
-import { Routes, Route } from "react-router-dom";   
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
 
-      <Routes> {/* REQUIRED */}
-        <Route 
-          path="/" 
-          element={
-            <>
-              <AddRecipeForm />
-              <RecipeList />
-            </>
-          } 
-        />
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <>
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
 
-        <Route path="/recipe/:id" element={<RecipeDetails />} /> {/* REQUIRED */}
-      </Routes>
-    </div>
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

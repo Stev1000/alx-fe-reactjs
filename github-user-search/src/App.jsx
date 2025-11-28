0b271251c4c7384d214cff16f3b5b1318a0f2899
Stevo <nsanzestevo1@gmail.com>
2025-11-28 11:05:38 +0200

    Task 0: Setup GitHub User Search Application using Vite + Axios

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import SearchBar from "./components/SearchBar";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>GitHub User Search</h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
